25182a9bbc6706a2e2381160965e33dff583ef04
sia <sia.shadpey@mail.utoronto.ca>
2020-07-31 15:28:44 -0400

    integrating with travis ci

---
+++ travis.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+keyword=$1
+
+
+if [ $keyword == build ]; then
+	mkdir build_tests
+	cd build_tests
+	cmake -DBUILD_TESTS=ON ..
+	make
+	cd ..
+elif [ $keyword == run ]; then
+	cd build_tests
+	ctest
+elif [ $keyword == coverage ]; then
+	gcovr -r .. "src"  --html-details -o cov.html
+else
+	echo "invalid keyword."
+fi
